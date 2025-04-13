6574d4ec0ee5d0c64776205dd70344e99d8f3284
sbx_user1051 <sbx_user1051@169.254.61.55>
2025-04-13 08:52:52 +0000

    Sun Apr 13 08:52:52 UTC 2025

--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -2,8 +2,6 @@ from lib.solutions.HLO.hello_solution import HelloSolution
 
 class TestHello():
     def test_hello(self):
-        assert HelloSolution().hello("John") == "Hello John"
-        assert HelloSolution().hello("Jane") == "Hello Jane"
-        assert HelloSolution().hello("Alice") == "Hello Alice"
-        assert HelloSolution().hello("Bob") == "Hello Bob"
-        assert HelloSolution().hello("Charlie") == "Hello Charlie"
+        assert HelloSolution().hello("Craftsman") == "Hello, World!"
+        assert HelloSolution().hello("Mr. X") == "Hello, World!"
+
