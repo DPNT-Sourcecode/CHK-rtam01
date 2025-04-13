c3770132116c01cabb1d9440164ef90f30ed76bf
sbx_user1051 <sbx_user1051@169.254.4.149>
2025-04-13 09:22:56 +0000

    Sun Apr 13 09:22:56 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,38 @@
+from dataclasses import dataclass
+from collections import Counter
+
+
+@dataclass
+class InventoryItem:
+    sku: str
+    price: int
+    bulk: int
+    bulk_price: int
+
 
 class CheckoutSolution:
 
+    def __init__(self):
+        # Define items
+        self.inventory = {
+            "A": InventoryItem("A", 50, 3, 130),
+            "B": InventoryItem("B", 30, 2, 45),
+            "C": InventoryItem("C", 20, 1, 0),
+            "D": InventoryItem("D", 15, 1, 0),
+        }
+
     # skus = unicode string
     def checkout(self, skus):
+        # Check for undefined invent
         if not isinstance(skus, list):
             return -1
+        if not all(isinstance(sku, str) and len(sku) == 1 for sku in skus):
+            return -1
+        
+
+        # Count occurrences
+        counts = Counter(skus)
+        total = 0
+
+        # Sum up total
+
--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -2,8 +2,17 @@ from lib.solutions.CHK.checkout_solution import CheckoutSolution
 
 
 class TestCheckout():
-    def test_invalid_input(self):
+    def test_non_list_input(self):
         # Test with invalid input
         assert CheckoutSolution().checkout("123") == -1
         assert CheckoutSolution().checkout("ABC") == -1
         assert CheckoutSolution().checkout("") == -1
+
+    def test_one_item(self):
+        assert CheckoutSolution().checkout(["A"]) == 50
+
+    def test_bulk_discount(self):
+        assert CheckoutSolution().checkout(["A, A, A"]) == 130
+
+    def assert_bulk_discount_and_extras(self):
+        assert CheckoutSolution().checkout(["A, A, A, A"]) == 180
